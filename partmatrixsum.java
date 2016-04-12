eb1ced359104187550ca5de482e4a3bdc6c27ca7
Weicesun <kevinsun181@gmail.com>
2016-04-11 22:23:28 -0400

    0411

--- partmatrixsum.java
+++ partmatrixsum.java
@@ -1,5 +1,5 @@
- private int[][] matrix;
-    private int[][] flag;
+public class NumMatrix {
+    private int[][] matrix;
     public NumMatrix(int[][] matrix) {
         this.matrix = matrix;
         for (int i = 0; i < matrix.length; i++) {
@@ -18,6 +18,13 @@
     }
 
     public int sumRegion(int row1, int col1, int row2, int col2) {
+        if (row1 > 0 && col1 > 0)
+        return (matrix[row2][col2] - matrix[row2][col1 - 1] - matrix[row1 - 1][col2] + matrix[row1 - 1][col1 - 1]);
+        if (row1 == 0 && col1 != 0)
+        return matrix[row2][col2] - matrix[row2][col1 - 1];
+        if (row1 > 0 && col1 == 0)
+        return matrix[row2][col2]  - matrix[row1 - 1][col2];
         
-        return matrix[row2][col2] - matrix[row2][col1] - matrix[row1][col2] + matrix[row1][col1];
+        return matrix[row2][col2];
     }
+}
