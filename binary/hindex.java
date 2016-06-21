81f5ea5f21125eb846a06f6572ad2f2ca45feb9a
Weicesun <kevinsun181@gmail.com>
2016-06-21 21:47:03 +0800

    h article that has at least h citations

--- binary/hindex.java
+++ binary/hindex.java
@@ -22,3 +22,28 @@ public int hIndex(int[] citations) {
         return len - end - 1;
     }
 // not correct
+//index2
+    public int hIndex(int[] citations) {
+        if (citations == null || citations.length == 0) {
+            return 0;
+        }
+        int start = 0;
+        int end = citations.length - 1;
+        int e = end;
+        while (start + 1 < end) {
+            int mid = start + (end - start) / 2;
+            if (citations[mid] < e - mid + 1) {
+                start = mid;
+            } else {
+                end = mid;
+            }
+        }
+        if (citations[start] >= e - start + 1) {
+            return e - start + 1;
+        }
+        if (citations[end] >= e - end + 1) {
+            return e - end + 1;
+        }
+        
+        return 0;
+    }
