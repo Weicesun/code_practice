0f8623747bef3a1ecbd21699d7cacd01b2f6c886
Weicesun <kevinsun181@gmail.com>
2016-07-23 20:36:53 +0800

    binary tree some idea

---
+++ binary/maxpath.java
@@ -0,0 +1,14 @@
+ int max = Integer.MIN_VALUE;
+    public int maxPathSum(TreeNode root) {
+        helper(root);
+        return max;
+    }
+    public int helper(TreeNode node) {
+        if (node == null) {
+            return 0;
+        }
+        int left = Math.max(0, helper(node.left));
+        int right = Math.max(0, helper(node.right));
+        max = Math.max(max, node.val + left + right);
+        return node.val + Math.max(left, right);
+    }
