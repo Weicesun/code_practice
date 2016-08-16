61cec7e7aa5e98f509b4dfc7477bc060e54c960d
Weicesun <kevinsun181@gmail.com>
2016-08-16 21:58:26 +0800

    array can also be view as linked list

---
+++ linkedlist/finddup.java
@@ -0,0 +1,19 @@
+public class Solution {
+    public int findDuplicate(int[] nums) {
+        if (nums.length > 1) {
+            int fast = nums[nums[0]];
+            int slow = nums[0];
+            while (fast != slow) {
+                fast = nums[nums[fast]];
+                slow = nums[slow];
+            }
+            fast = 0;
+            while (fast != slow) {
+                fast = nums[fast];
+                slow = nums[slow];
+            }
+            return slow;
+        }
+        return -1;
+    }
+}
