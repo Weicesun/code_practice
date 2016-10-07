81c19a2c7e0a2e4c680bfa4f096d6556f5fec534
Weicesun <kevinsun181@gmail.com>
2016-10-06 23:23:33 -0400

    backpack problem 1-3

---
+++ backpack/3.java
@@ -0,0 +1,13 @@
+public int backPackIII(int[] A, int[] V, int m) {
+        // Write your code here
+          int[][] dp = new int[A.length + 1][m + 1];
+       for (int i = 1; i < dp.length; i++) {
+           for (int j = 1; j < dp[0].length; j++) {
+               dp[i][j] = dp[i - 1][j];
+               if (j >= A[i - 1]) {
+                   dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - A[i - 1]] + V[i - 1]);
+               }
+           }
+       }
+       return dp[dp.length - 1][dp[0].length - 1];
+    }
